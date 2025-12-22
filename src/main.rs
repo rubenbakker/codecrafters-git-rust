a01ec426329919816eec60b0810e324ca3c6b2b4
Ruben Bakker <ruben@uncomplex.ch>
2025-12-22 18:09:30 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::anyhow;
-use flate2::Compression;
 use flate2::read::ZlibDecoder;
 use flate2::write::ZlibEncoder;
+use flate2::Compression;
 use sha1::{Digest, Sha1};
 #[allow(unused_imports)]
 use std::env;
@@ -25,44 +25,12 @@ fn main() -> anyhow::Result<()> {
         } else if args[1] == "cat-file" {
             if args.len() > 3 && args[2] == "-p" {
                 let hash = args[3].to_string();
-                let file_path = get_path_for_hash(hash.as_str())?;
-                let file_path_str = file_path.to_str().unwrap();
-                let mut file = File::open(file_path_str)?;
-                let mut data = vec![];
-                file.read_to_end(&mut data)?;
-                let mut zlib_decoder = ZlibDecoder::new(&data[..]);
-                let mut result = String::new();
-                zlib_decoder.read_to_string(&mut result)?;
-                if result.starts_with("blob") {
-                    let result_str = result.as_str();
-                    let parts: Vec<&str> = result_str.split("\0").collect();
-                    let content = result.get(parts[0].len() + 1..).unwrap();
-                    print!("{}", content);
-                }
+                cat_file(hash)?;
             }
         } else if args[1] == "hash-object" {
             if args.len() > 3 && args[2] == "-w" {
                 let path = args[3].to_string();
-                let mut file = File::open(path.as_str())?;
-                let mut content: Vec<u8> = vec![];
-                file.read_to_end(&mut content)?;
-                let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-                let mut full_content : Vec<u8> = vec![];
-                let header = format!("blob {}\0", content.len()).as_bytes().to_vec().clone();
-                full_content.write_all(header.as_slice())?;
-                full_content.write_all(content.as_slice())?;
-                let hash = Sha1::digest(&full_content);
-                let hash = base16ct::lower::encode_string(&hash);
-                e.write_all(full_content.as_ref())?;
-                let compressed = e.finish()?;
-                let dir_path = get_dir_for_hash(hash.as_str())?;
-                if !dir_path.exists() {
-                    let _result = fs::create_dir(dir_path)?;
-                }
-                let output_file_path = get_path_for_hash(hash.as_str())?;
-                let mut output_file = File::create(output_file_path)?;
-                output_file.write_all(compressed.as_ref())?;
-                print!("{}", hash);
+                hash_object(path)?
             }
         } else {
             println!("unknown command: {}", args[1]);
@@ -71,6 +39,49 @@ fn main() -> anyhow::Result<()> {
         println!("Usage init or cat-file");
     }
     Ok(())
+}
+
+fn cat_file(hash: String) -> anyhow::Result<()> {
+    let file_path = get_path_for_hash(hash.as_str())?;
+    let file_path_str = file_path.to_str().unwrap();
+    let mut file = File::open(file_path_str)?;
+    let mut data = vec![];
+    file.read_to_end(&mut data)?;
+    let mut zlib_decoder = ZlibDecoder::new(&data[..]);
+    let mut result = String::new();
+    zlib_decoder.read_to_string(&mut result)?;
+    if result.starts_with("blob") {
+        let result_str = result.as_str();
+        let parts: Vec<&str> = result_str.split("\0").collect();
+        let content = result.get(parts[0].len() + 1..).unwrap();
+        print!("{}", content);
+    }
+    Ok(())
+}
+
+fn hash_object(path: String) -> anyhow::Result<()> {
+    let mut file = File::open(path.as_str())?;
+    let mut content: Vec<u8> = vec![];
+    file.read_to_end(&mut content)?;
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    let mut full_content : Vec<u8> = vec![];
+    let header = format!("blob {}\0", content.len()).as_bytes().to_vec().clone();
+    full_content.write_all(header.as_slice())?;
+    full_content.write_all(content.as_slice())?;
+    let hash = Sha1::digest(&full_content);
+    let hash = base16ct::lower::encode_string(&hash);
+    e.write_all(full_content.as_ref())?;
+    let compressed = e.finish()?;
+    let dir_path = get_dir_for_hash(hash.as_str())?;
+    if !dir_path.exists() {
+        let _result = fs::create_dir(dir_path)?;
+    }
+    let output_file_path = get_path_for_hash(hash.as_str())?;
+    let mut output_file = File::create(output_file_path)?;
+    output_file.write_all(compressed.as_ref())?;
+    print!("{}", hash);
+    Ok(())
+
 }
 
 fn get_dir_for_hash(hash: &str) -> anyhow::Result<PathBuf> {
